671ca2161d91c6229e85b35f6337cbcce36e6b73
Joshua Schmidt <jschmidtnj@gmail.com>
2018-03-31 14:01:29 -0400

    Created current data file

--- end-script.sh
+++ end-script.sh
@@ -7,6 +7,7 @@ file_name=data.csv
 current_time=$(date +"%Y-%m-%d-%H-%M-%S")
 new_filename=$current_time-$file_name
 mv $file_name /home/pi/Desktop/StevensBajaSAE/data/$new_filename
+cat /home/pi/Desktop/StevensBajaSAE/data/$new_filename > /home/pi/Desktop/StevensBajaSAE/data/current_data_file
 cd /home/pi/Desktop/StevensBajaSAE
 git add -A
 git commit -m "added data files"
