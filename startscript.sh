543db7fe76220bf663cda9a20387df31463765b1
¨jschmidtnj¨ <jschmidtnj@gmail.com>
2018-04-12 20:21:16 -0500

    added data files

--- startscript.sh
+++ startscript.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 #OCCURS ON STARTUP OF PI
 
-xinput --set-prop 'FT5406 memory based driver' 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
-sudo su
-brightness=30
-echo $brightness > /sys/class/backlight/rpi_backlight/brightness
-exit
 cd /home/pi/Desktop/StevensBajaSAE
 git pull origin master
-git add -A
-git commit -m "added data files"
-git push origin master
 sudo mount /dev/sda1 /mnt/usb/
-python3 /home/pi/Desktop/StevensBajaSAE/app-1.py
 python3 /home/pi/Desktop/StevensBajaSAE/app-1.py
